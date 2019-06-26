e93beaa2b76b57bef61a3e1baab0eddc3ed99b93
lidf077 <lidf077@gmail.com>
2019-06-26 22:47:23 +0800

    线性表的实现

--- src/main/java/com/google/learn/adt/list/ArrList.java
+++ src/main/java/com/google/learn/adt/list/ArrList.java
@@ -1,8 +1,6 @@
 package com.google.learn.adt.list;
 
-import com.google.learn.adt.queue.LinkedQueue;
-
-import java.util.List;
+import java.util.Arrays;
 
 public class ArrList<T> implements ListInterface<T> {
     private T[] list;
@@ -18,6 +16,7 @@ public class ArrList<T> implements ListInterface<T> {
             checkCapacity(initialCapacity);
         }
 
+        // 为什么要生成一个比客户指定容量大1的数组，请看ensureCapacity()方法
         @SuppressWarnings("unchecked")
         T[] tempList = (T[]) new Object[initialCapacity + 1];
         list = tempList;
@@ -27,30 +26,89 @@ public class ArrList<T> implements ListInterface<T> {
 
     private void checkCapacity(int capacity) {
         if (capacity > MAX_CAPACITY) {
-            throw new IllegalStateException("Attempt to create a queue whose capacity exceeds allowed maximun of " + MAX_CAPACITY);
+            throw new IllegalStateException("Attempt to create a list whose capacity exceeds allowed maximun of " + MAX_CAPACITY);
         }
     }
 
     private void checkInitialization() {
         if (!initialized) {
-            throw new SecurityException("ArrayQueue object is not initialized properly");
+            throw new SecurityException("ArrayList object is not initialized properly");
         }
     }
 
-
     @Override
     public void add(T newEntry) {
+        checkInitialization();
+        list[numberOfEntries] = newEntry;
+        numberOfEntries++;
+        // 为什么要在添加后才对数组扩容，请查看ensureCapacity()方法的注释
+        /**
+         * 包 栈 队列中，添加前扩容，如果容量比较大，客户就要等待
+         * 可以在初始化数组时，就初始化一个比客户指定容量大1的数组
+         * 而且ensureCapacity()方法会在数组还有一个位置时就扩容
+         */
+        ensureCapacity();
 
+        /**
+         * add(numberOfEntries, newEntry);另一种实现
+         */
+    }
+
+    private void ensureCapacity() {
+        int capacity = list.length - 1;
+        if (numberOfEntries >= capacity) {
+            int newCapacity = capacity * 2;
+            checkCapacity(newCapacity);
+            list = Arrays.copyOf(list, newCapacity + 1);
+        }
     }
 
     @Override
     public void add(int newPosition, T newEntry) {
+        checkInitialization();
+        if ((newPosition >= 0) && (newPosition < numberOfEntries)) {
+            if (newPosition < numberOfEntries) {
+                makeRoom(newPosition);
+            }
+            list[newPosition] = newEntry;
+            numberOfEntries++;
+            ensureCapacity();
+        } else {
+            throw new IndexOutOfBoundsException("Given position of add's new entry is out of bounds");
+        }
+    }
 
+    private void makeRoom(int newPosition) {
+        assert (newPosition >= 0) && (newPosition < numberOfEntries);
+
+        int lastIndex = numberOfEntries;
+        for (int index = lastIndex; index >= newPosition; index--) {
+            list[index + 1] = list[index];
+        }
     }
 
     @Override
     public T remove(int givenPosition) {
-        return null;
+        checkInitialization();
+        if ((givenPosition >= 0) && (givenPosition < numberOfEntries)) {
+            assert !isEmpty();
+            T result = list[givenPosition];
+            removeGap(givenPosition);
+
+            numberOfEntries--;
+            return result;
+        } else {
+            throw new IndexOutOfBoundsException("Illegal position given to remove operation.");
+        }
+    }
+
+    private void removeGap(int givenPosition) {
+        assert (givenPosition >= 0) && (givenPosition < numberOfEntries);
+
+        int lastIndex = numberOfEntries;
+        for (int index = givenPosition; index < lastIndex; index++) {
+            list[index] = list[index + 1];
+        }
     }
 
     @Override
@@ -60,31 +118,62 @@ public class ArrList<T> implements ListInterface<T> {
 
     @Override
     public T replace(int givenPosition, T newEntry) {
-        return null;
+        checkInitialization();
+        if ((givenPosition >= 0) && (givenPosition < numberOfEntries)) {
+            assert !isEmpty();
+            T originalEntry = list[givenPosition];
+            list[givenPosition] = newEntry;
+            return originalEntry;
+        } else {
+            throw new IndexOutOfBoundsException("Illegal position given to replace operation.");
+        }
     }
 
     @Override
     public T getEntry(int givenPosition) {
-        return null;
+        checkInitialization();
+        if ((givenPosition >= 0) && (givenPosition < numberOfEntries)) {
+            assert !isEmpty();
+            return list[givenPosition];
+        } else {
+            throw new IndexOutOfBoundsException("Illegal position given to getEntry operation.");
+        }
     }
 
     @Override
     public T[] toArray() {
-        return null;
+        checkInitialization();
+
+        @SuppressWarnings("unchecked")
+        T[] result = (T[]) new Object[numberOfEntries];
+        for (int index = 0; index < numberOfEntries; index++) {
+            result[index] = list[index];
+        }
+        return result;
     }
 
     @Override
     public boolean contains(T anEntry) {
-        return false;
+        checkInitialization();
+        boolean found = false;
+        int index = 0;
+
+        while (!found && (index < numberOfEntries)) {
+            if (anEntry.equals(list[index])) {
+                found = true;
+            }
+            index++;
+        }
+        return found;
     }
 
     @Override
     public int getLength() {
-        return 0;
+        return numberOfEntries;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return numberOfEntries == 0;
     }
 }
